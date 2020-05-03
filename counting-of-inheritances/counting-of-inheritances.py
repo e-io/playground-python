c5ab0e58bfd04dad34836c62f92090861fe674ca
Pavel Mikhailov <62296354+e-io@users.noreply.github.com>
2020-05-03 14:14:58 +0300

    code is changed. but runtime error

--- counting-of-inheritances/counting-of-inheritances.py
+++ counting-of-inheritances/counting-of-inheritances.py
@@ -1,23 +1,39 @@
 import json
 
-classes = json.load(open("classes2.json"))
-# classes = json.loads(input())
+# classes = json.load(open("classes2.json"))
+classes = json.loads(input())
 
 inheritances = dict()
 for class_ in classes:
     name = class_['name']
+
     if name not in inheritances:
-        inheritances[name] = 1
-    else:
-        inheritances[name] += 1
+        inheritances[name] = set()
 
     for parent in class_['parents']:
         if parent not in inheritances:
-            inheritances[parent] = 1
+            inheritances[parent] = set(name)
         else:
-            inheritances[parent] += 1
+            inheritances[parent].add(name)
 
+
+def all_children(parent):
+    children = inheritances[parent]
+    all_children_ = set()
+    for child in children:
+        all_children_ .add(child)
+        results = all_children(child)
+        for result in results:
+            all_children_ .add(result)
+    return all_children_
+
+
+for parent in inheritances:
+    all_children_ = set()
+    all_children_ = all_children(parent)
+    inheritances[parent] = all_children_
+    
 list_ = list(inheritances.keys())
 list_.sort()
-for class_ in list_:
-    print(class_, ':', inheritances[class_])
+for parent in list_:
+    print(parent, ':', len(inheritances[parent]) + 1)
