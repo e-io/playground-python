7232457ca12b09e99035f2c9404494ef8093c64e
earthman <ur.ay@ya.ru>
2021-11-08 16:46:22 +0300

    now it works! Like version 0.1. a whole module and a test.

--- interests-for-vk-requirements/list2block/list2block.py
+++ interests-for-vk-requirements/list2block/list2block.py
@@ -6,6 +6,45 @@ def str2block(string, width=40):
     result = f'{{:_^{max}}}'.format(string)
     return(result)
 
-def list2block(json='', width=40):
-    return 42
+def list2block(md='', width=40):
     print("I am 'list2block'")
+    return 42
+
+
+
+def md2block(input, output, width=40):
+
+    with (
+            open(input, "r") as md,
+            open(output, "w") as txt,
+    ):
+
+            txt.writelines([str2block('', width=width), '\n'])
+
+            lines = md.read().split('\n')
+            for line in lines:
+
+                match line.split(' ', 1):
+                    case '#', essence:
+                        line = '<' + essence.upper() + '>'
+                    case '##'|'###', essence:
+                        line = essence.upper()
+                    case '-'|'*', essence:
+                        line = essence
+                    case beginning, essence:
+                        # all other beginnigs treat as part of essence. No action needed (line == line)
+                        pass
+                    case _:
+                        pass
+                result = str2block(line, width=width)
+
+                txt.writelines ([result, '\n'])
+
+            txt.write(str2block('', width=width))
+
+            print('i am here')
+
+
+
+
+
--- interests-for-vk-requirements/tests/test_convert_interests_to_block.py
+++ interests-for-vk-requirements/tests/test_convert_interests_to_block.py
@@ -1,12 +1,14 @@
-from list2block.list2block import list2block, str2block
+from list2block import list2block, str2block, md2block
 
 
 def test_call_of_function():
     print('')
     print(str2block('the begin of output'))
-    num = list2block()
 
+    num = list2block()
     print(num)
+
+    md2block("interests.md", "vk_interests_block.txt")
     print(str2block('the end of output'))
 
 
